5ddd7279db26d7dec0c50edfc013f9040151d9db
unknown <sanyapridava@mail.ru>
2024-08-16 19:34:07 +0300

    Fix bug with stop runtime

--- Learn/src/main/java/org/example/Main.java
+++ Learn/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import Cars.BMV;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Run Java-project is succes!");
+        //Ввод данных
+        Scanner in = new Scanner(System.in);
+
+        String model, owner;
+        Integer born;
+        model = in.nextLine();
+        born = parseInt(in.nextLine());
+        owner = in.nextLine();
+
+        BMV my_car = new BMV(model, born, owner);
+        System.out.println(my_car.getInfo()[2]);
     }
 }
