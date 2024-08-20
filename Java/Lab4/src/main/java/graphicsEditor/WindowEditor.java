982e91736cbabe9c169851ccc12c2cad89933564
unknown <sanyapridava@mail.ru>
2024-08-20 14:52:54 +0300

    Методы для добавления полотна и панели кнопок

--- Java/Lab4/src/main/java/graphicsEditor/WindowEditor.java
+++ Java/Lab4/src/main/java/graphicsEditor/WindowEditor.java
@@ -1,9 +1,12 @@
 package graphicsEditor;
 
+import graphicsEditor.components.CanvasEditor;
+import graphicsEditor.components.PanelButtons;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class WindowEditor extends JFrame {
+public class WindowEditor {
     private static WindowEditor root = null;
 
     public static WindowEditor start(String title, int width, int height) {
@@ -22,10 +25,46 @@ public class WindowEditor extends JFrame {
                 JFrame root = new JFrame(title);
                 root.setSize(width, height);
 
+                //Добавление полотна для рисования фигур
+                addCanvas(root);
+
+                //Панель с кнопками для интерфеса задания рисования фигур
+                addPanelButtons(root);
+
                 root.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 root.setVisible(true);
             }
         });
+
+    }
+
+    private void addCanvas(JFrame root) {
+        root.setContentPane(new CanvasEditor());
+    }
+
+    private void addPanelButtons(JFrame root) {
+        PanelButtons panel = new PanelButtons();
+
+        Font big_words = new Font("TimesRoman", Font.BOLD, 20);
+
+        JButton button = new JButton("Линия");
+        button.setFont(big_words);
+        panel.add(button);
+
+        button = new JButton("Квадрат");
+        button.setFont(big_words);
+        panel.add(button);
+
+        button = new JButton("Круг");
+        button.setFont(big_words);
+        panel.add(button);
+
+        button = new JButton("Овал");
+        button.setFont(big_words);
+        panel.add(button);
+
+
+        root.add(panel);
 
     }
 }
